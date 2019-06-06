99af206eaa122c239afd537174d263d75fdbb92b
Paul Makowski <pm@polyswarm.io>
2019-06-06 14:19:23 -0700

    autoapi_dir -> autoapi_dirs

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ extensions = [
 
 # Document Python Code
 autoapi_type = 'python'
-autoapi_dir = '../src'
+autoapi_dirs = ['../src']
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
