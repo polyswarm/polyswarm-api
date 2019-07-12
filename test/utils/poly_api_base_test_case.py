baf676cd0721f39b9fa55a9171f138f868dab692
Marcio Gualtieri <marcio.gualtieri@polyswarm.io>
2019-07-12 10:01:17 -0400

    Using single quotes

--- test/utils/poly_api_base_test_case.py
+++ test/utils/poly_api_base_test_case.py
@@ -8,17 +8,17 @@ class PolyApiBaseTestCase(AioHTTPTestCase):
     def __init__(self, *args, **kwargs):
         super(PolyApiBaseTestCase, self).__init__(*args, **kwargs)
         self.test_query = {
-            "query": {
-                "exists": {
-                    "field": "lief.libraries"
+            'query': {
+                'exists': {
+                    'field': 'lief.libraries'
                 }
             }
         }
-        self.test_api_key = "317b21cb093263b701043cb0831a53b9"
+        self.test_api_key = '317b21cb093263b701043cb0831a53b9'
 
     @staticmethod
     def _get_test_text_resource(resource):
-        return resource_string("test.resources", resource).decode("utf-8")
+        return resource_string('test.resources', resource).decode('utf-8')
 
     def _get_test_json_resource(self, resource):
         return json.loads(self._get_test_text_resource(resource))
