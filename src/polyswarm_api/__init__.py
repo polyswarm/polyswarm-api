2e4257254fcd8f6ebc136a03c7a1ba81dc2a4dc8
sam <samuel.neto@polyswarm.io>
2019-07-12 13:47:44 -0300

    sending all arguments to the wrapped object

--- src/polyswarm_api/__init__.py
+++ src/polyswarm_api/__init__.py
@@ -789,8 +789,7 @@ class PolyswarmAsyncAPI(object):
 class PolyswarmAPI(object):
     """A synchronous interface to the public and private PolySwarm APIs."""
 
-    def __init__(self, key, uri='https://api.polyswarm.network/v1', get_limit=100,
-                 post_limit=1000, timeout=600, force=False, community='lima', check_version=True):
+    def __init__(self, *args, **kwargs):
         """
 
         :param key: PolySwarm API key
@@ -802,7 +801,8 @@ class PolyswarmAPI(object):
         :param community: Community to scan against.
         :param check_version: Whether or not to check Github for version information
         """
-        self.ps_api = PolyswarmAsyncAPI(key, uri, get_limit, post_limit, timeout, force, community, check_version)
+        # Since this is a wrapper, send all arguments down to the async client
+        self.ps_api = PolyswarmAsyncAPI(*args, **kwargs)
         self.loop = asyncio.get_event_loop()
 
     def set_force(self, force):
