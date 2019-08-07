eaa2b9711a6ad448fedbd0b54271ce601c795e14
sam <samuel.neto@polyswarm.io>
2019-08-07 14:35:32 -0300

    fixing tests

--- test/client/client_search_query__test.py
+++ test/client/client_search_query__test.py
@@ -18,7 +18,8 @@ class SearchQueryTestCase(PolyApiBaseTestCase):
 
         async def not_found_response(request):
             del request
-            return web.Response(text='{}', content_type='application/json', status=404)
+            response = self._get_test_text_resource('search_query_server_success_empty_response.json')
+            return web.Response(text=response, content_type='application/json')
 
         async def invalid_query_response(request):
             del request
