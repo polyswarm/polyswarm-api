2f7ec033e8665be37ec2baaaba4c762b7f7cb3e2
Ben Schmidt <bens@polyswarm.io>
2019-08-14 08:50:23 -0400

    reduce the defaults on get/post

--- src/polyswarm_api/__init__.py
+++ src/polyswarm_api/__init__.py
@@ -24,7 +24,7 @@ class PolyswarmAsyncAPI(object):
     """
 
     def __init__(self, key, uri='https://api.polyswarm.network/v1', get_limit=100,
-                 post_limit=1000, timeout=600, force=False, community='lima', check_version=True):
+                 post_limit=1, timeout=600, force=False, community='lima', check_version=True):
         """
 
         :param key: PolySwarm API key
@@ -799,7 +799,7 @@ class PolyswarmAPI(object):
     """A synchronous interface to the public and private PolySwarm APIs."""
 
     def __init__(self, key, uri='https://api.polyswarm.network/v1', get_limit=100,
-                 post_limit=1000, timeout=600, force=False, community='lima', check_version=True):
+                 post_limit=1, timeout=600, force=False, community='lima', check_version=True):
         """
 
         :param key: PolySwarm API key
