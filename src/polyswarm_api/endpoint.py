07f2e371beee24c15bbda0c30ce8d2c3ae5d5690
sam <samuel.neto@polyswarm.io>
2019-10-31 19:43:35 -0300

    reorganizing endpoints, allowing different executors to be used

--- src/polyswarm_api/endpoint.py
+++ src/polyswarm_api/endpoint.py
@@ -1,10 +1,25 @@
-from . import const, utils
-from requests.exceptions import HTTPError
+from concurrent import futures
+
+from . import const
+from . import utils
+from . import http
+from .types import result
+
+
+class PolyswarmRequest(object):
+    """This class holds a requests-compatible dictionary and extra infor we need to parse the reponse."""
+    def __init__(self, api_instance, request_parameters, output_file=None, result=None):
+        self.api_instance = api_instance
+        self.request_parameters = request_parameters
+        self.output_file = output_file
+        self.result = result
+        self.raw_result = None
 
 
 class PolyswarmRequestGenerator(object):
     """ This class will return requests-compatible arguments for the API """
-    def __init__(self, uri, community):
+    def __init__(self, api_instance, uri, community):
+        self.api_instance = api_instance
         self.uri = uri
         self.community = community
 
@@ -19,96 +34,141 @@ class PolyswarmRequestGenerator(object):
         self.hash_search_fmt = '{}/{}/{}'
 
     def download(self, h, fh):
-        return {
-            'method': 'GET',
-            'url': self.download_fmt.format(self.download_base, h.hash_type, h.hash),
-            'stream': True,
-            'output_file': fh,
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': self.download_fmt.format(self.download_base, h.hash_type, h.hash),
+                'stream': True,
+            },
+            output_file=fh,
+            result=result.DownloadResult,
+        )
 
     def download_archive(self, u, fh):
         """ This method is special, in that it is simply for downloading from S3 """
-        return {
-            'method': 'GET',
-            'url': u,
-            'stream': True,
-            'output_file': fh,
-            'headers': {'Authorization': None}
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': u,
+                'stream': True,
+                'headers': {'Authorization': None}
+            },
+            output_file=fh,
+            result=result.DownloadResult,
+        )
 
     def stream(self, since=const.MAX_SINCE_TIME_STREAM):
-        return {
-            'method': 'GET',
-            'url': '{}/download/stream'.format(self.consumer_base),
-            'params': {'since': since},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/download/stream'.format(self.consumer_base),
+                'params': {'since': since},
+            },
+        )
 
     def search_hash(self, h, with_instances=True, with_metadata=True):
-        return {
-            'method': 'GET',
-            'url': self.search_base,
-            'params': {
-                'type': h.hash_type,
-                'hash': h.hash,
-                'with_instances': utils.bool_to_int[with_instances],
-                'with_metadata': utils.bool_to_int[with_metadata]
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': self.search_base,
+                'params': {
+                    'type': h.hash_type,
+                    'hash': h.hash,
+                    'with_instances': utils.bool_to_int[with_instances],
+                    'with_metadata': utils.bool_to_int[with_metadata]
+                },
             },
-        }
+        )
 
     def search_metadata(self, q, with_instances=True, with_metadata=True):
-        return {
-            'method': 'GET',
-            'url': self.search_base,
-            'params': {
-                'type': 'metadata',
-                'with_instances': utils.bool_to_int[with_instances],
-                'with_metadata': utils.bool_to_int[with_metadata]
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': self.search_base,
+                'params': {
+                    'type': 'metadata',
+                    'with_instances': utils.bool_to_int[with_instances],
+                    'with_metadata': utils.bool_to_int[with_metadata]
+                },
+                'json': q.query,
             },
-            'json': q.query,
-        }
+        )
 
     def submit(self, artifact):
-        return {
-            'method': 'POST',
-            'url': self.community_base,
-            'files': {
-                'file': (artifact.artifact_name, artifact.file_handle),
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'POST',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': self.community_base,
+                'files': {
+                    'file': (artifact.artifact_name, artifact.file_handle),
+                },
+                # very oddly, when included in files parameter this errors out
+                'data': {'artifact-type': artifact.artifact_type.name}
             },
-            # very oddly, when included in files parameter this errors out
-            'data': {'artifact-type': artifact.artifact_type.name}
-        }
+            result=result.SubmitResult(artifact, polyswarm=self.api_instance)
+        )
 
     def rescan(self, h, **kwargs):
-        return {
-            'method': 'POST',
-            'url': '{}/rescan/{}/{}'.format(self.community_base, h.hash_type, h.hash)
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'POST',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/rescan/{}/{}'.format(self.community_base, h.hash_type, h.hash)
+            },
+        )
 
     def lookup_uuid(self, uuid, **kwargs):
-        return {
-            'method': 'GET',
-            'url': '{}/uuid/{}'.format(self.community_base, uuid)
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/uuid/{}'.format(self.community_base, uuid)
+            },
+            result=result.ScanResult(polyswarm=self.api_instance)
+        )
 
     def _get_engine_names(self):
-        return {
-            'method': 'GET',
-            'url': '{}/microengines/list'.format(self.uri),
-            'headers': {'Authorization': None},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/microengines/list'.format(self.uri),
+                'headers': {'Authorization': None},
+            },
+        )
 
     def submit_live_hunt(self, rule):
-        return {
-            'method': 'POST',
-            'url': '{}/live'.format(self.hunt_base),
-            'json': {'yara': rule.ruleset},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'POST',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/live'.format(self.hunt_base),
+                'json': {'yara': rule.ruleset},
+            },
+        )
 
     def live_lookup(self, with_bounty_results=True, with_metadata=True,
                     limit=const.RESULT_CHUNK_SIZE, offset=0, id=None,
                     since=0):
         req = {
             'method': 'GET',
+            'timeout': const.DEFAULT_HTTP_TIMEOUT,
             'url': '{}/live/results'.format(self.hunt_base),
             'params': {
                 'with_bounty_results': utils.bool_to_int[with_bounty_results],
@@ -122,20 +182,25 @@ class PolyswarmRequestGenerator(object):
         if id:
             req['params']['id'] = id
 
-        return req
+        return PolyswarmRequest(req)
 
     def submit_historical_hunt(self, rule):
-        return {
-            'method': 'POST',
-            'url': '{}/historical'.format(self.hunt_base),
-            'json': {'yara': rule.ruleset},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'POST',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/historical'.format(self.hunt_base),
+                'json': {'yara': rule.ruleset},
+            },
+        )
 
     def historical_lookup(self, with_bounty_results=True, with_metadata=True,
                           limit=const.RESULT_CHUNK_SIZE, offset=0, id=None,
                           since=0):
         req = {
             'method': 'GET',
+            'timeout': const.DEFAULT_HTTP_TIMEOUT,
             'url': '{}/historical/results'.format(self.hunt_base),
             'params': {
                 'with_bounty_results': utils.bool_to_int[with_bounty_results],
@@ -149,116 +214,112 @@ class PolyswarmRequestGenerator(object):
         if id:
             req['params']['id'] = id
 
-        return req
+        return PolyswarmRequest(req)
 
     def historical_delete(self, hunt_id):
-        return {
-            'method': 'DELETE',
-            'url': '{}/historical'.format(self.hunt_base),
-            'params': {'hunt_id': hunt_id}
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'DELETE',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/historical'.format(self.hunt_base),
+                'params': {'hunt_id': hunt_id}
+            },
+        )
 
     def live_delete(self, hunt_id):
-        return {
-            'method': 'DELETE',
-            'url': '{}/live'.format(self.hunt_base),
-            'params': {'hunt_id': hunt_id}
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'DELETE',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/live'.format(self.hunt_base),
+                'params': {'hunt_id': hunt_id}
+            },
+        )
 
     def historical_list(self):
-        return {
-            'method': 'GET',
-            'url': '{}/historical'.format(self.hunt_base),
-            'params': {'all': 'true'},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/historical'.format(self.hunt_base),
+                'params': {'all': 'true'},
+            },
+        )
 
     def live_list(self):
-        return {
-            'method': 'GET',
-            'url': '{}/live'.format(self.hunt_base),
-            'params': {'all': 'true'},
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/live'.format(self.hunt_base),
+                'params': {'all': 'true'},
+            },
+        )
 
     def score(self, uuid):
-        return {
-            'method': 'GET',
-            'url': '{}/submission/{}/polyscore'.format(self.consumer_base, uuid)
-        }
+        return PolyswarmRequest(
+            self.api_instance,
+            {
+                'method': 'GET',
+                'timeout': const.DEFAULT_HTTP_TIMEOUT,
+                'url': '{}/submission/{}/polyscore'.format(self.consumer_base, uuid)
+            },
+        )
 
 
 class PolyswarmRequestExecutor(object):
     """ This class accepts requests from a PolyswarmRequestGenerator and executes it """
-    def __init__(self, session=None, timeout=const.DEFAULT_HTTP_TIMEOUT):
-        self.session = session
-        self.timeout = timeout
+    def __init__(self, key, session=None):
+        self.session = session or http.PolyswarmHTTP(key, retries=const.DEFAULT_RETRIES)
+        self.requests = []
 
-    def execute(self, request):
-        if 'timeout' not in request:
-            request['timeout'] = self.timeout
+    def _request(self, request):
+        request.raw_result = self.session.request(**request.request_parameters)
+        request.raw_result.raise_for_status()
 
-        # this is a special case for handling output to a file
-        output = request.get('output_file', None)
+        if request.output_file:
+            for chunk in request.raw_result.iter_content(chunk_size=const.DOWNLOAD_CHUNK_SIZE):
+                request.output_file.write(chunk)
 
-        if 'output_file' in request:
-            del request['output_file']
+        if request.result:
+            request.result.parse_result(request.raw_result)
 
-        req = self.session.request(**request)
+        return request
 
-        if output:
-            try:
-                return self._download_to_fh(req, output)
-            except HTTPError:
-                return req
+    def push(self, request):
+        raise NotImplementedError()
 
-        return req
-
-    def _download_to_fh(self, req, fh):
-        raise NotImplementedError
+    def execute(self):
+        raise NotImplementedError()
 
 
 class PolyswarmFuturesExecutor(PolyswarmRequestExecutor):
-    def _download_to_fh(self, req, fh):
-        # this is unfortunately the cleanest way I think I can do this with requests-futures
-        # derived partially from https://github.com/ross/requests-futures/issues/54
-        def do_download(r, f):
-            for chunk in r.iter_content(chunk_size=const.DOWNLOAD_CHUNK_SIZE):
-                f.write(chunk)
-            return r
-        resp = req.result()
-        resp.raise_for_status()
-        return self.session.executor.submit(do_download, resp, fh)
+    def __init__(self, key):
+        self.executor = futures.ThreadPoolExecutor(const.DEFAULT_WORKER_COUNT)
+        super(PolyswarmFuturesExecutor, self).__init__(key)
+
+    def push(self, request):
+        self.requests.append(self.executor.submit(self._request, request))
+
+    def execute(self):
+        requests = self.requests
+        # flush before looping in case we have nested executions
+        self.requests = []
+        for future in futures.as_completed(requests):
+            yield future.result()
 
 
 class PolyswarmSynchronousExecutor(PolyswarmRequestExecutor):
-    def _download_to_fh(self, req, fh):
-        req.raise_for_status()
-        for chunk in req.iter_content(chunk_size=const.DOWNLOAD_CHUNK_SIZE):
-            fh.write(chunk)
-        return req
+    def push(self, request):
+        self.requests.append(request)
 
-
-class PolyswarmEndpoint(object):
-    """ This is the PolyswarmEndpoint class, that handles talking with the PolySwarm API"""
-    def __init__(self,  request_generator=None, request_executor=None):
-        self.generator = request_generator
-        self.executor = request_executor
-
-    def __getattr__(self, name):
-        """
-        This function is the black magic behind PolyswarmEndpoint. The goal of the function is to
-        return a callable that chains together a RequestGenerator and a RequestExecutor, but to do
-        this dynamically so that we don't have to manually define a new method for every possible
-        RequestGenerator method.
-
-        As such, we return a closure here, and this closure simply calls the given function name
-        in the RequestGenerator, and passes this into the RequestExecutor's execute function to
-        be run. The end result is a simple calling convention for all methods supported by
-        the provided RequestGenerator, e.g. endpoint.search() -> returns a result.
-
-
-        :param name: unresolved attribute name
-        :return: closure that calls the RequestGenerator then executes it with a RequestExecutor
-        """
-        def endpoint_wrapper(*args, **kwargs):
-            return self.executor.execute(getattr(self.generator, name)(*args, **kwargs))
-        return endpoint_wrapper
+    def execute(self):
+        responses = []
+        for request in self.requests:
+            responses.append(self._request(request))
+        self.requests = []
+        return responses
