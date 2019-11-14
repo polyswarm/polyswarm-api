1fd74b4889f37c3d6d1dd936f979d994e989d6a4
sam <samuel.neto@polyswarm.io>
2019-11-14 18:55:16 -0300

    release 2.0.0.dev0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='polyswarm-api',
-    version='1.1.1',
+    version='2.0.0.dev0',
     description='Client library to simplify interacting with the PolySwarm consumer API',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/polyswarm_api/_version.py
+++ src/polyswarm_api/_version.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.1'
+__version__ = '2.0.0.dev0'
 __release_url__ = 'https://api.github.com/repos/polyswarm/polyswarm-api/releases/latest'
 
 
