e9fafc42244141b70ed69b0eecd09668d49fc691
sam <samuel.neto@polyswarm.io>
2020-02-19 19:42:34 -0300

    fixing the permalink

--- src/polyswarm_api/const.py
+++ src/polyswarm_api/const.py
@@ -4,7 +4,7 @@ from . import _version
 
 # API constants
 DEFAULT_GLOBAL_API = 'https://api.polyswarm.network/v2'
-DEFAULT_PERMALINK_BASE = os.getenv('POLYSWARM_PORTAL_URI', 'https://polyswarm.network/scan/results')
+DEFAULT_PERMALINK_BASE = os.getenv('POLYSWARM_PORTAL_URI', 'https://polyswarm.network/scan/results/file')
 DEFAULT_COMMUNITY = 'default'
 DEFAULT_SCAN_TIMEOUT = 60*15
 RESULT_CHUNK_SIZE = 100
