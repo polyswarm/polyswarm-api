f0cba666771b07a7bfee45a19b88f6d357e212a1
Ben Schmidt <bens@polyswarm.io>
2021-12-05 23:47:44 -0500

    Bump version: 2.9.2 → 2.10.0

--- src/polyswarm_api/__init__.py
+++ src/polyswarm_api/__init__.py
@@ -1,5 +1,5 @@
 # https://www.python.org/dev/peps/pep-0008/#module-level-dunder-names
-__version__ = '2.9.2'
+__version__ = '2.10.0'
 __release_url__ = 'https://api.github.com/repos/polyswarm/polyswarm-api/releases/latest'
 
 from . import api
