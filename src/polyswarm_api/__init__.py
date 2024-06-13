55c4f9be9786fd049ea83df894cc6598922b619d
Samuel Barbosa <samuel.neto@polyswarm.io>
2024-06-13 19:34:04 -0300

    Bump version: 3.7.1 → 3.8.0

--- src/polyswarm_api/__init__.py
+++ src/polyswarm_api/__init__.py
@@ -1,5 +1,5 @@
 # https://www.python.org/dev/peps/pep-0008/#module-level-dunder-names
-__version__ = '3.7.1'
+__version__ = '3.8.0'
 __release_url__ = 'https://api.github.com/repos/polyswarm/polyswarm-api/releases/latest'
 
 from . import api
